25e4f7217518815da649319d59c2e113934fd4d9
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-11-24 18:46:43 -0300

    feat: input para busca

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
+import dadosMock from './assets/sampleWeather.json';
+import imagens from './assets/images.index.js';
 import { useState, useEffect } from 'react';
-import casaco from "./assets/casaco.png"
+import casaco from './assets/casaco.png'
 import styled from 'styled-components';
+import lupa from './assets/lupa.png'
 import Swal from 'sweetalert2';
 import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [req, setReq] = useState('')
-  const [url_mock, setUrl_mock] = useState('https://jsonplaceholder.typicode.com/photos')
-  
-  useEffect(() => {
-    /*
-    axios.get(url_mock)
-      .then(res => {
-        console.log(res.data)
-        setReq(res.data)
-      })
-      .catch(err => {
-        Swal.fire({
-          title: 'Erro!',
-          text: 'Houve um problema ao obter os dados da página :(',
-          icon: 'error',
-          confirmButtonText: 'Cool'
-        })
-      })
-    */
-  }, [url_mock])
+  const [req, setReq] = useState('');
+  const [data, setData] = useState({})
+  const [inputBusca, setInputBusca] = useState('')
 
+  useEffect(() => {
+    console.log(dadosMock);
+    console.log(imagens);
+    setData(dadosMock);
+  }, [])
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      console.log('Tecla Enter pressionada');
+    }
+  };
   return (
     <Body>
       <SideMenu>
         <LogoDiv>
-          <img 
-            src={casaco} 
+          <img
+            src={casaco}
             alt="Logomarca 'Levo um casasquinho' TM "
             title="Logomarca 'Levo um casasquinho' TM "
           />
@@ -40,9 +36,22 @@ function App() {
             Levo um casaquinho?
           </p>
         </LogoDiv>
-        <div>
-          Aqui ficará o menu lateral
-        </div>
+        <DivBusca>
+          <img 
+            src={lupa} 
+            alt='Realize sua busca!' 
+            title='Realize sua busca' 
+            onClick={() => handleKeyDown({key: 'Enter'})}
+          />
+          <InputBusca
+            placeholder="Procure por uma cidade"
+            type="text"
+            value={inputBusca}
+            onChange={(e) => setInputBusca(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        </DivBusca>
+
       </SideMenu>
       <Dashboard>
         <div>
@@ -65,13 +74,67 @@ const Body = styled.div`
 const SideMenu = styled.div`
   height: 100vh;
   width: 35vw;
-  padding: 4.2vmin;
+  padding-top: 4vmin;
+  gap: 10px;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
   background-color: white;
 `
+
+const LogoDiv = styled.div`
+  padding: 4.2vmin;
+  height: 120px;
+  width: 100%;
+  gap: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 10vmin;
+    width: 10vmin;
+  }
+  p {
+    font-family: 'Poppins', 'sans-serif';
+    font-size: 4vmin;
+    font-weight: 700;
+    text-align: left;
+    color: #222222;
+  }
+`
+const DivBusca = styled.div`
+  border: 1px red solid;
+  position: relative;
+  padding: 4.2vmin;
+  width: 100%;
+  height: 100px;
+  padding: 10px;
+  img { 
+    cursor: pointer;
+    position: absolute;
+    top: 30px;
+    left: 20px;
+  }
+`
+const InputBusca = styled.input`
+  width: 100%;
+  height: 80px;
+  text-align: center;
+  border-radius: 24px;
+  color: #424243;
+  background-color: #EDEDEF;
+  border: 0px solid lightgray;
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: 0em;
+  &:focus {
+    outline: none;
+  }
+`
+
+
 const Dashboard = styled.div`
   height: 100vh;
   width: 65vw;
@@ -80,26 +143,4 @@ const Dashboard = styled.div`
   justify-content: center;
   align-items: center;
 `
-const LogoDiv = styled.div`
-  height: 120px;
-  width: 100%;
-  gap: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  img {
-    width: 10vw;
-    border: 1px red solid;
-  }
-  p {
-    font-family: 'Poppins', sans-serif;
-    font-size: 3.2vw;
-    font-weight: 600;
-    text-align: left;
-    color: #222222;
-    border: 1px red solid;
-  }
-`
-
-
 export default App
---
+++ src/assets/images.index.js
@@ -0,0 +1,41 @@
+import d01 from "./01d.png"
+import n01 from "./01n.png"
+import d02 from "./02d.png"
+import n02 from "./02n.png"
+import d03 from "./03d.png"
+import n03 from "./03n.png"
+import d04 from "./04d.png"
+import n04 from "./04n.png"
+import d09 from "./09d.png"
+import n09 from "./09n.png"
+import d10 from "./10d.png"
+import n10 from "./10n.png"
+import d11 from "./11d.png"
+import n11 from "./11n.png"
+import d13 from "./13d.png"
+import n13 from "./13n.png"
+import d50 from "./50d.png"
+import n50 from "./50n.png"
+
+const imagens = {
+    "01d": d01,
+    "01n": n01,
+    "02d": d02,
+    "02n": n02,
+    "03d": d03,
+    "03n": n03,
+    "04d": d04,
+    "04n": n04,
+    "09d": d09,
+    "09n": n09,
+    "10d": d10,
+    "10n": n10,
+    "11d": d11,
+    "11n": n11,
+    "13d": d13,
+    "13n": n13,
+    "50d": d50,
+    "50n": n50
+}
+
+export default imagens
